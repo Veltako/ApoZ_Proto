98c203e1d11891959c0aede4ad9b4aa29d0e9c1c
Baptiste Dupache <baptistedupache@gmail.com>
2025-07-21 14:02:00 +0200

    ajout du crouch + réplication (bug coté client)

--- Plugins/ApoZLocomotion/Source/ApoZLocomotion/Private/LocomotionComponent.cpp
+++ Plugins/ApoZLocomotion/Source/ApoZLocomotion/Private/LocomotionComponent.cpp
@@ -11,30 +11,59 @@ ULocomotionComponent::ULocomotionComponent()
 // Sprint
 void ULocomotionComponent::StartSprinting()
 {
-    bIsSprinting = true;
-    if (ACharacter* Char = Cast<ACharacter>(GetOwner()))
+    if (!GetOwner() || !GetOwner()->HasAuthority())
     {
-        Char->GetCharacterMovement()->MaxWalkSpeed = 1200.f;
-        UE_LOG(LogTemp, Warning, TEXT("MaxWalkSpeed = %f"), Char->GetCharacterMovement()->MaxWalkSpeed);
+        // Demande au serveur d'exécuter la fonction
+        Server_StartSprinting();
+        return;
+    }
+
+    bIsSprinting = true;
+    if (ACharacter* OwnerChar = Cast<ACharacter>(GetOwner()))
+    {
+        OwnerChar->GetCharacterMovement()->MaxWalkSpeed = 1200.f;
+        UE_LOG(LogTemp, Warning, TEXT("MaxWalkSpeed (sprint) = %f"), OwnerChar->GetCharacterMovement()->MaxWalkSpeed);
     }
 }
 
 void ULocomotionComponent::StopSprinting()
 {
-    bIsSprinting = false;
-    if (ACharacter* Char = Cast<ACharacter>(GetOwner()))
+    if (!GetOwner() || !GetOwner()->HasAuthority())
     {
-        Char->GetCharacterMovement()->MaxWalkSpeed = 400.f;
+        // Demande au serveur d'exécuter la fonction
+        Server_StopSprinting();
+        return;
+    }
+
+    bIsSprinting = false;
+    if (ACharacter* OwnerChar = Cast<ACharacter>(GetOwner()))
+    {
+        OwnerChar->GetCharacterMovement()->MaxWalkSpeed = 400.f;
+        UE_LOG(LogTemp, Warning, TEXT("MaxWalkSpeed (unsprint) = %f"), OwnerChar->GetCharacterMovement()->MaxWalkSpeed);
     }
 }
 
-// Crounch
+void ULocomotionComponent::Server_StartSprinting_Implementation()
+{
+    StartSprinting();
+}
+
+void ULocomotionComponent::Server_StopSprinting_Implementation()
+{
+    StopSprinting();
+}
+
+// --------- CROUCH ---------
 void ULocomotionComponent::StartCrouching()
 {
     bIsCrouching = true;
     if (ACharacter* OwnerChar = Cast<ACharacter>(GetOwner()))
     {
+        // Active pour la nav agent
+        OwnerChar->GetCharacterMovement()->GetNavAgentPropertiesRef().bCanCrouch = true;
+        // Active pour le movement en général
         OwnerChar->Crouch();
+        OwnerChar->GetCharacterMovement()->MaxWalkSpeedCrouched = 200.f;
     }
 }
 
@@ -47,11 +76,21 @@ void ULocomotionComponent::StopCrouching()
     }
 }
 
-//  Réplication
+void ULocomotionComponent::Server_StartCrouching_Implementation()
+{
+    StartCrouching();
+}
+
+void ULocomotionComponent::Server_StopCrouching_Implementation()
+{
+    StopCrouching();
+}
+
+// --------- REPLICATION ---------
 void ULocomotionComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
     Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
     DOREPLIFETIME(ULocomotionComponent, bIsSprinting);
     DOREPLIFETIME(ULocomotionComponent, bIsCrouching);
 }
--- Plugins/ApoZLocomotion/Source/ApoZLocomotion/Public/LocomotionComponent.h
+++ Plugins/ApoZLocomotion/Source/ApoZLocomotion/Public/LocomotionComponent.h
@@ -2,10 +2,10 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "Net/UnrealNetwork.h"
+#include "Net/UnrealNetwork.h" // Pour la réplication
 #include "LocomotionComponent.generated.h"
 
-UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class APOZLOCOMOTION_API ULocomotionComponent : public UActorComponent
 {
     GENERATED_BODY()
@@ -13,25 +13,37 @@ class APOZLOCOMOTION_API ULocomotionComponent : public UActorComponent
 public:
     ULocomotionComponent();
 
-    // Etat du sprint (répliqué)
-    UPROPERTY(Replicated, BlueprintReadOnly, Category = "Locomotion")
+    // === SPRINt ===
+    UPROPERTY(Replicated, BlueprintReadOnly, Category="Locomotion")
     bool bIsSprinting = false;
 
-    UFUNCTION(BlueprintCallable, Category = "Locomotion")
+    UFUNCTION(BlueprintCallable, Category="Locomotion")
     void StartSprinting();
 
-    UFUNCTION(BlueprintCallable, Category = "Locomotion")
+    UFUNCTION(BlueprintCallable, Category="Locomotion")
     void StopSprinting();
 
-    // Etat du crouch (répliqué)
-    UPROPERTY(Replicated, BlueprintReadOnly, Category = "Locomotion")
+    UFUNCTION(Server, Reliable)
+    void Server_StartSprinting();
+
+    UFUNCTION(Server, Reliable)
+    void Server_StopSprinting();
+
+    // === CROUCH ===
+    UPROPERTY(Replicated, BlueprintReadOnly, Category="Locomotion")
     bool bIsCrouching = false;
 
-    UFUNCTION(BlueprintCallable, Category = "Locomotion")
+    UFUNCTION(BlueprintCallable, Category="Locomotion")
     void StartCrouching();
 
-    UFUNCTION(BlueprintCallable, Category = "Locomotion")
+    UFUNCTION(BlueprintCallable, Category="Locomotion")
     void StopCrouching();
+
+    UFUNCTION(Server, Reliable)
+    void Server_StartCrouching();
+
+    UFUNCTION(Server, Reliable)
+    void Server_StopCrouching();
 
 protected:
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
